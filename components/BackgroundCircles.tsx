795a9a513f51a88b296feffb06dfdc0d0103bdd9
CodeXMalinga <rdimalingadesilva@gmail.com>
2022-10-25 22:42:01 +0530

    Hero Section Done

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -1,16 +1,35 @@
 import React from "react";
+import { motion } from 'framer-motion';
 
 type Props = {};
 
 const BackgroundCircles = (props: Props) => {
   return (
-  <div className="relative flex items-center justify-center">
+  <motion.div
+    initial={
+      {
+        opacity: 0,
+      }
+    }
+    animate={
+      {
+        scale:[1,2,2,3,1],
+        opacity: [0.1, 0.2, 0.4, 0.8, 0.4],
+        borderRadius: ['20%', '20%', '50%', '80%', '20%'],
+      }
+    }
+    transition={
+      {
+        duration: 3.5,
+      }
+    }
+    className="relative flex items-center justify-center">
     <div className="absolute border border-[#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
     <div className="absolute border border-[#333333] rounded-full h-[300px] w-[300px] mt-52" />
     <div className="absolute border border-[#333333] rounded-full h-[500px] w-[500px] mt-52" />
-    <div className="absolute border border-[#478778] rounded-full opacity-20 h-[650px] w-[650px] animate-pulse mt-52" />
-    <div className="absolute border border-[#333333]-500 rounded-full h-[800px] w-[800px] mt-52" />
-  </div>
+    <div className="absolute border border-[#afbe24] rounded-full opacity-20 h-[650px] w-[650px] animate-pulse mt-52" />
+    <div className="absolute border border-[#333333] rounded-full h-[800px] w-[800px] mt-52" />
+  </motion.div>
   );
 };
 
